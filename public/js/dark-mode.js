ded8bf0b715d36ca1691c4a676bacd8ffca66f95
Kirti Singla <singla.kirti2002@gmail.com>
2021-10-05 20:41:46 +0530

    corrected

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -1,11 +1,17 @@
 $(document).ready(() => {
   var body = document.querySelector("body");
-  if (localStorage.dark == true) body.classList.add("dark-mode");
+  var toggleButton = document.getElementById("toggle");
+  if (localStorage.dark == true) {
+    body.classList.add("dark-mode");
+    toggleButton.classList.toggle("active");
+  } else {
+    toggleButton.classList.toggle("toggle");
+  }
 
   //   var heading = document.querySelector("h1");
   //   var select = document.querySelector("select");
   //   var input = document.querySelector("input");
-  var toggleButton = document.getElementById("toggle");
+
   toggleButton.onclick = function () {
     toggleButton.classList.toggle("active");
     body.classList.toggle("dark-mode");
