0735d8b1ee9f526d3bf1b687a093afb7c6ddbc8e
raghavgoel25 <>
2021-10-10 13:10:11 +0530

    Added Formatting Features
    Added feature to sanitise the text entered by user and to apply markdown formatting

--- utils/message.js
+++ utils/message.js
@@ -1,10 +1,11 @@
+const marked=require("marked");
+const encode=require("html-entities")['encode'];
 const moment = require("moment");
 function formatMessages(username,text){
     return{
         username,
-        text,
+        text: marked(encode(text)),
         time: moment().valueOf()
-
     };
 }
 module.exports = formatMessages;
