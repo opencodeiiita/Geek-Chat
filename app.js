af29889de3c9a5989b6ae71bff5b6837621e2703
Shailesh Tiwari <iib2020001@iiita.ac.in>
2021-10-11 20:50:53 +0400

    Bot messages now show differently
    Update main.css

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
-const cors = require("cors");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,106 +10,92 @@ const io = socketio(server);
 var usrnm;
 var room;
 
-const formatMessages = require("./utils/message.js");
-const { usersArr, newUser } = require("./utils/users.js");
-const { on } = require("events");
+const formatMessages = require('./utils/message.js');
+const { usersArr, newUser } = require('./utils/users.js');
+const { on } = require('events');
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/main.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/main.html');
 });
 
-app.post("/", (req, res) => {
-    usrnm = req.body.usrnm;
-    room = req.body.room;
+app.post('/', (req, res) => {
+  usrnm = req.body.usrnm;
+  room = req.body.room;
 
-    //Each user should have a unique username in a particular room
-    if (usersArr.find(user => {
-        if (user.name === usrnm && user.room === room) return true;
-    })) {
-        return res.sendFile(__dirname + "/public/index.html");
-    }
+  //Each user should have a unique username in a particular room
+  if (
+    usersArr.find(user => {
+      if (user.name === usrnm && user.room === room) return true;
+    })
+  ) {
+    return res.sendFile(__dirname + '/public/index.html');
+  }
 
-    res.sendFile(__dirname + "/public/main.html");
+  res.sendFile(__dirname + '/public/main.html');
 });
 
-io.on("connection", (socket) => {
-    if (usrnm != undefined && room != undefined) {
-        newUser(usrnm, room, socket.id);
-        socket.join(room);
-        socket.emit("roomJoined", { room, username: usrnm });
+io.on('connection', socket => {
+  if (usrnm != undefined && room != undefined) {
+    newUser(usrnm, room, socket.id);
+    socket.join(room);
+    socket.emit('roomJoined', { room, username: usrnm });
+  }
+
+  socket.emit('announcement', formatMessages('GeekChat Bot', 'Welcome to GeekChat!'));
+  socket.to(room).emit('announcement', formatMessages('GeekChat Bot', `${usrnm} entered the chat!`));
+  let userList = usersArr.filter(ob => ob.room === room);
+  // console.log(userList);
+  socket.emit('userList', userList);
+  socket.to(room).emit('userJoined', { id: socket.id, username: usrnm });
+
+  socket.on('disconnect', () => {
+    io.in(room).emit('userLeft', { id: socket.id, username: usrnm });
+    let userIndex = -1,
+      user;
+    for (const [index, userObj] of usersArr.entries()) {
+      if (userObj.session_id === socket.id) {
+        userIndex = index;
+        user = userObj;
+        break;
+      }
     }
+    if (userIndex !== -1) {
+      usersArr.splice(userIndex, 1);
+      io.in(user.room).emit('announcement', formatMessages('GeekChat Bot', `${user.name} disconnected.`));
+    }
+  });
 
-    socket.emit(
-        "message",
-        formatMessages("", "GeekChat Bot", "Welcome to GeekChat!", "")
-    );
-    socket
-        .to(room)
-        .emit(
-            "message",
-            formatMessages("", "GeekChat Bot", `${usrnm} entered the chat!`, "")
-        );
-    let userList = usersArr.filter((ob) => ob.room === room);
-    // console.log(userList);
-    socket.emit("userList", userList);
-    socket.to(room).emit("userJoined", { id: socket.id, username: usrnm });
+  socket.on('chatMessage', messageObject => {
+    let user = usersArr.find(ob => ob.session_id === socket.id);
+    console.log(messageObject, messageObject.msg);
+    // console.log(formatMessages(user.name, msg));
 
-    socket.on("disconnect", () => {
-        io.in(room).emit("userLeft", { id: socket.id, username: usrnm });
-        let userIndex = -1,
-            user;
-        for (const [index, userObj] of usersArr.entries()) {
-            if (userObj.session_id === socket.id) {
-                userIndex = index;
-                user = userObj;
-                break;
-            }
-        }
-        if (userIndex !== -1) {
-            usersArr.splice(userIndex, 1);
-            io.in(user.room).emit(
-                "message",
-                formatMessages("", "GeekChat Bot", `${user.name} disconnected.`, "")
-            );
-        }
-    });
-
-    socket.on("chatMessage", (messageObject) => {
-        let user = usersArr.find((ob) => ob.session_id === socket.id);
-        console.log(messageObject, messageObject.msg);
-        // console.log(formatMessages(user.name, msg));
-
-        let messageID = user.name+'_'+ (new Date()).getTime();
-        if (user) {
-            // console.log(user.name, messageObject["text"], messageObject.userID);
-            io.in(user.room).emit(
-                "message",
-                formatMessages(
-                    messageID,
-                    user.name,
-                    messageObject.msg,
-                    messageObject.userID
-                )
-            );
-        }
-    });
-    socket.on("deleteChatMsg", (msgId) => {
-        if (msgId == null || msgId == undefined){
-            return;
-        }
-        let user = usersArr.find(ob => ob.session_id === socket.id);
-        if (user) {
-            io.in(user.room).emit("deleteMsgFromChat", msgId);
-        }
-    });
+    let messageID = user.name + '_' + new Date().getTime();
+    if (user) {
+      // console.log(user.name, messageObject["text"], messageObject.userID);
+      io.in(user.room).emit(
+        'message',
+        formatMessages(messageID, user.name, messageObject.msg, messageObject.userID)
+      );
+    }
+  });
+  socket.on('deleteChatMsg', msgId => {
+    if (msgId == null || msgId == undefined) {
+      return;
+    }
+    let user = usersArr.find(ob => ob.session_id === socket.id);
+    if (user) {
+      io.in(user.room).emit('deleteMsgFromChat', msgId);
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
--- public/js/index.js
+++ public/js/index.js
@@ -1,75 +1,87 @@
 // import { usersArr, newUser } from "../utils/users.js";
 
 const socket = io();
-const roomName = document.querySelector("#room-name");
-const users = document.querySelector("#users");
-const roomNameDiv = document.querySelector(".room-name-container");
+const roomName = document.querySelector('#room-name');
+const users = document.querySelector('#users');
+const roomNameDiv = document.querySelector('.room-name-container');
 const userMap = new Map();
-const CURRENT_USER = sessionStorage.getItem('current_user')
+const CURRENT_USER = sessionStorage.getItem('current_user');
 var room;
-socket.on("roomJoined", (connectionObj) => {
-    console.log(connectionObj);
-    room = connectionObj.room;
-    roomName.innerHTML = connectionObj.room;
-    roomNameDiv.classList.remove("animate");
+socket.on('roomJoined', connectionObj => {
+  console.log(connectionObj);
+  room = connectionObj.room;
+  roomName.innerHTML = connectionObj.room;
+  roomNameDiv.classList.remove('animate');
 });
-socket.on("message", (message) => {
-    let userID = socket.id;
-    outputMessage({ message, userID });
-    console.log({ message, userID });
+socket.on('message', message => {
+  let userID = socket.id;
+  outputMessage({ message, userID });
+  console.log({ message, userID });
+});
+socket.on('announcement', announcement => {
+  outputAnnouncement(announcement);
 });
 
 function outputMessage(msg) {
-    var values = Object.values(msg);
-    console.log(msg.message);
-    console.log(values[0]);
-    const div = document.createElement("div");
-    div.setAttribute('id', values[0].id)
-    if (values[0].userID === values[1]) {
-        div.classList.add("author");
-        div.innerHTML += `<button class="btn-danger" onclick="deleteMsg('${values[0].id}')"><span class="material-icons">
+  var values = Object.values(msg);
+  console.log(msg.message);
+  console.log(values[0]);
+  const div = document.createElement('div');
+  div.setAttribute('id', values[0].id);
+  if (values[0].userID === values[1]) {
+    div.classList.add('author');
+    div.innerHTML += `<button class="btn-danger" onclick="deleteMsg('${values[0].id}')"><span class="material-icons">
         delete
-        </span></button>`
-    } else {
-        div.classList.add("message");
-    }
-    if (values[0].username === "GeekChat Bot") {
-        div.innerHTML += `<p class="meta">${values[0].username} <span>${moment(
-            values[0].time
-        ).format("h:mm a")}</span></p>
+        </span></button>`;
+  } else {
+    div.classList.add('message');
+  }
+  if (values[0].username === 'GeekChat Bot') {
+    div.innerHTML += `<p class="meta">${values[0].username} <span>${moment(values[0].time).format(
+      'h:mm a'
+    )}</span></p>
 <p class="text">
-${values[0]["text"]}
+${values[0]['text']}
 </p>`;
-    } else {
-        div.innerHTML += `<p class="meta">${values[0].username} <span>${moment(
-            values[0].time
-        ).format("h:mm a")}</span></p>
+  } else {
+    div.innerHTML += `<p class="meta">${values[0].username} <span>${moment(values[0].time).format(
+      'h:mm a'
+    )}</span></p>
 <p class="text">
   ${values[0].text}
 </p>`;
-    }
+  }
 
-    document.querySelector(".chat-messages").appendChild(div);
-    scrollToBottom();
+  document.querySelector('.chat-messages').appendChild(div);
+  scrollToBottom();
 }
 
-const form = document.getElementById("chat-form");
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+function outputAnnouncement(Annc) {
+  const div = document.createElement('div');
+  div.setAttribute('id', 'announcement');
+  div.classList.add('announcement');
+  console.log(Annc);
+  div.innerHTML = Annc.username + `<span>${moment(Annc.time).format('h:mm a')}</span>`;
+  document.querySelector('.chat-messages').appendChild(div);
+  scrollToBottom();
+}
 
-    const msg = e.target.elements.msg.value;
-    console.log(socket.id, msg);
-    let userID = socket.id;
-    socket.emit("chatMessage", { msg, userID });
+const form = document.getElementById('chat-form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
 
-    e.target.elements.msg.value = "";
-    e.target.elements.msg.focus();
-    scrollToBottom();
+  const msg = e.target.elements.msg.value;
+  console.log(socket.id, msg);
+  let userID = socket.id;
+  socket.emit('chatMessage', { msg, userID });
+
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+  scrollToBottom();
 });
 
 function scrollToBottom() {
-    document.querySelector(".chat-messages").scrollTop =
-        document.querySelector(".chat-messages").scrollHeight;
+  document.querySelector('.chat-messages').scrollTop = document.querySelector('.chat-messages').scrollHeight;
 }
 // document.ready(() => {
 //   var toggleButton = document.getElementById("toggle");
@@ -78,45 +90,45 @@ function scrollToBottom() {
 //   };
 // });
 
-socket.on("userList", (userList) => {
-    for (let { name, session_id } of userList) {
-        userMap.set(session_id, name);
-        let user = document.createElement("li");
-        user.classList.add("fade-in");
-        user.dataset.id = session_id;
-        user.innerHTML = name;
-        users.appendChild(user);
-    }
-});
-socket.on("userJoined", ({ id, username }) => {
-    userMap.set(id, username);
-    const user = document.createElement("li");
-    user.classList.add("fade-in");
-    user.dataset.id = id;
-    user.innerHTML = username;
+socket.on('userList', userList => {
+  for (let { name, session_id } of userList) {
+    userMap.set(session_id, name);
+    let user = document.createElement('li');
+    user.classList.add('fade-in');
+    user.dataset.id = session_id;
+    user.innerHTML = name;
     users.appendChild(user);
-});
-
-socket.on("userLeft", ({ id, username }) => {
-    const user = document.querySelector(`[data-id="${id}"]`);
-    if (user) user.classList.add("fade-out");
-    setTimeout(() => {
-        user.remove();
-    }, 1500);
-    userMap.delete(id, username);
-});
-
-socket.on('deleteMsgFromChat', (msgId) => {
-    if (msgId == null || msgId == undefined) {
-      return;
-    }
-    document.getElementById(msgId).remove();
-});
-
-function deleteMsg(info){
-    console.log(info)
-    let [name, id] = info.split('_');
-    if (name === CURRENT_USER) {
-      socket.emit('deleteChatMsg', info);
-    }
   }
+});
+socket.on('userJoined', ({ id, username }) => {
+  userMap.set(id, username);
+  const user = document.createElement('li');
+  user.classList.add('fade-in');
+  user.dataset.id = id;
+  user.innerHTML = username;
+  users.appendChild(user);
+});
+
+socket.on('userLeft', ({ id, username }) => {
+  const user = document.querySelector(`[data-id="${id}"]`);
+  if (user) user.classList.add('fade-out');
+  setTimeout(() => {
+    user.remove();
+  }, 1500);
+  userMap.delete(id, username);
+});
+
+socket.on('deleteMsgFromChat', msgId => {
+  if (msgId == null || msgId == undefined) {
+    return;
+  }
+  document.getElementById(msgId).remove();
+});
+
+function deleteMsg(info) {
+  console.log(info);
+  let [name, id] = info.split('_');
+  if (name === CURRENT_USER) {
+    socket.emit('deleteChatMsg', info);
+  }
+}
