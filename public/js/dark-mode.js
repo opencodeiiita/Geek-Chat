8ff852de6b53cc9eb9b1f60402ce95a37616eac3
raghavgoel25 <>
2021-10-26 15:24:31 +0530

    Modified Styling

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -1,16 +1,16 @@
  $(document).ready(() => {
-  var body = document.querySelector("body");
-  var toggleButton = document.getElementById("toggle");
-  if (localStorage.dark == true) {
-    body.classList.add("dark-mode");
-    toggleButton.classList.toggle("active");
-  } else {
-    toggleButton.classList.toggle("toggle");
-  }
+     var body = document.querySelector("body");
+     var toggleButton = document.getElementById("toggle");
+     if (localStorage.dark == true) {
+         body.classList.add("dark-mode");
+         toggleButton.classList.toggle("active");
+     } else {
+         toggleButton.classList.toggle("toggle");
+     }
 
-  toggleButton.onclick = function () {
-    toggleButton.classList.toggle("active");
-    body.classList.toggle("dark-mode");
-    localStorage.dark ^= true;
-  };
-});
+     toggleButton.onclick = function() {
+         toggleButton.classList.toggle("active");
+         body.classList.toggle("dark-mode");
+         localStorage.dark ^= true;
+     };
+ });
