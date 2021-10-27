e993f46bf23eb1f9330f9b765c4fe659446d1bbb
Ksathwik03 <iit2020241@iiita.ac.in>
2021-10-28 00:54:40 +0530

    final changes

--- utils/message.js
+++ utils/message.js
@@ -4,12 +4,22 @@ const moment = require("moment");
 var Filter = require('bad-words')
 const filter = new Filter({ placeHolder: 'x'});
 
+function sanitize(message){
+    const fiterMessage = filter.clean(message)
+    const markdown = marked(fiterMessage)
+    const reducedString = markdown.replace( /(<([^>]+)>)/ig, '')
+    if(filter.isProfane(reducedString)){
+        return null
+    }
+    return fiterMessage
+}
+
 function formatMessages(id, username, text, userID, profilePhoto='') {
     if(text.includes('replied-msg-container')){
     return {
         id,
         username,
-        text: filter.clean(marked(text).toString().replace( /(<([^>]+)>)/ig, '')),
+        text: sanitize(text) || "Profanity not allowed",
         userID,
         time: moment().valueOf(),
         profilePhoto,
@@ -18,7 +28,7 @@ function formatMessages(id, username, text, userID, profilePhoto='') {
 return {
     id,
     username,
-    text: filter.clean(marked(encode(text)).toString().replace( /(<([^>]+)>)/ig, '')),
+    text: sanitize(encode(text)) || "Profanity not allowed",
     userID,
     time: moment().valueOf(),
     profilePhoto,
