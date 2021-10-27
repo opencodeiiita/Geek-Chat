2c4e6e0ed0b574f66e9b0986d65339dabf2fe551
Ksathwik03 <iit2020241@iiita.ac.in>
2021-10-27 20:03:09 +0530

    Bad words

--- utils/message.js
+++ utils/message.js
@@ -1,12 +1,15 @@
 const marked = require("marked");
 const encode = require("html-entities")["encode"];
 const moment = require("moment");
+var Filter = require('bad-words')
+const filter = new Filter({ placeHolder: 'x'});
+
 function formatMessages(id, username, text, userID, profilePhoto='') {
     if(text.includes('replied-msg-container')){
     return {
         id,
         username,
-        text: marked(text),
+        text: filter.clean(marked(text).toString().replace( /(<([^>]+)>)/ig, '')),
         userID,
         time: moment().valueOf(),
         profilePhoto,
@@ -15,7 +18,7 @@ function formatMessages(id, username, text, userID, profilePhoto='') {
 return {
     id,
     username,
-    text: marked(encode(text)),
+    text: filter.clean(marked(encode(text)).toString().replace( /(<([^>]+)>)/ig, '')),
     userID,
     time: moment().valueOf(),
     profilePhoto,
